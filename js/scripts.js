b0d6d71583594d1a8ffde8e63d3cd086ebc820f4
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-27 11:37:20 +0200

    commit 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // il software deve chiedere per  5 volte di inserire un numero
 var richiestaNumero= prompr("inserisci per 5 volte un numero")
+console.log(richiestaNumero);
+alert(richiestaNumero);
 
-for
+for (var i = 0; i < 5; i++);
