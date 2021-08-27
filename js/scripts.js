a56ff9e4121e38518c37248712f3fd04a4cd08fa
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-27 12:20:41 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-// il software deve chiedere per  5 volte di inserire un numero
-var richiestaNumero= prompt("inserisci per 5 volte un numero");
-console.log(richiestaNumero);
-alert(richiestaNumero);
-
-for (var i = 0; i < 6; i++) {
+// esercizio 2
+var inserisciNumero = prompt("inserisci un munero");
     
+for (var i= 0; i<100; i++) {
+    if (i % 2 == 0);
+    console.log(i);
 }
+
+
+
+
+
+
+
+
+
+
+
+// // il software deve chiedere per  5 volte di inserire un numero
+// var richiestaNumero= prompt("inserisci per 5 volte un numero");
+// console.log(richiestaNumero);
+// alert(richiestaNumero);
+
+// for (var i = 0; i < 6; i++) {
+    
+// }
