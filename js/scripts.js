f02e7e60c131d9766cb78dd22f90af76949b6d1f
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-27 11:57:25 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // il software deve chiedere per  5 volte di inserire un numero
-var richiestaNumero= prompt("inserisci per 5 volte un numero")
+var richiestaNumero= prompt("inserisci per 5 volte un numero");
 console.log(richiestaNumero);
 alert(richiestaNumero);
 
 for (var i = 0; i < 6; i++) {
-    var somma = prompt(richiestaNumero);
-   
+    
 }
