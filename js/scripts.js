60804c5fd5b42bf29f9b44fa6f855514c6057d9f
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-27 11:40:29 +0200

    correzione promt, errore indentazione

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // il software deve chiedere per  5 volte di inserire un numero
-var richiestaNumero= prompr("inserisci per 5 volte un numero")
+var richiestaNumero= prompt("inserisci per 5 volte un numero")
 console.log(richiestaNumero);
 alert(richiestaNumero);
 
 for (var i = 0; i < richiestaNumero.lenght; i++) {
     var somma = somma + richiestaNumero.lenght;
-    
+    console.log(somma);
 }
