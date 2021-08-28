094b666f4bdb28cba54931640f9a513795fbf50a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-28 17:05:51 +0200

    commit

--- snack1/js/scripts.js
+++ snack1/js/scripts.js
@@ -1,14 +1,16 @@
 // esercizio 1
 
-// il software deve chiedere per  5 volte di inserire un numero
-
-var richiestaNumero= prompt("inserisci per 5 volte un numero");
-console.log(richiestaNumero);
-alert(richiestaNumero);
-
-for (var i = 0; i < 6; i++) {
-
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+for (var i= 0; i < 5; i++){
+    var numero= prompt("inserisci il numero per 5 volte");
+    var somma= somma + numero;
+    console.log(numero);
 }
+console.log(somma);
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
 
 
 
--- snack4/js/scripts.js
+++ snack4/js/scripts.js
@@ -1,3 +1,4 @@
-// snack 4
+// esercizio 4
+
 // Crea un array di numeri interi
 // e fai la somma di tutti gli elementi che sono in posizione(indice dell'array) dispari
