4daced79271baa24e1e42abc6c6cf07e3276f28a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-28 17:18:24 +0200

    commit

--- snack1/js/scripts.js
+++ snack1/js/scripts.js
@@ -1,13 +1,14 @@
 // esercizio 1
-
+var somma= 0;
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 for (var i= 0; i < 5; i++){
-    var numero= prompt("inserisci il numero per 5 volte");
+    var numero= parseInt(prompt("inserisci il numero per 5 volte"));
     var somma= somma + numero;
     console.log(numero);
 }
 console.log(somma);
 // Il programma stampa la somma di tutti i numeri inseriti.
+
 // Esegui questo programma in due versioni, con il for e con il while.
 
 
