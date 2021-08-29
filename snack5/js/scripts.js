9b722fb430276be521b5b29f5f7f1bde0c82481d
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-29 11:23:34 +0200

    commit snack 5

--- snack5/js/scripts.js
+++ snack5/js/scripts.js
@@ -3,3 +3,7 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
+var elementiUno =["rosso", 1, "giallo", 2,];
+var elementiDue = ["verde", 3, "blu", 4, "bianco"];
+console.log(elementiUno);
+console.log(elementiDue);
