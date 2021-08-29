a6edcd3c5d3139353f8d54295e7c7dbb675f8183
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-29 16:47:07 +0200

    commit

--- snack2/js/scripts.js
+++ snack2/js/scripts.js
@@ -2,3 +2,12 @@
 
 // inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
+
+for (var i= 0; i<7; i++){
+    var inserisciNumero = parseInt(prompt("inserisci un Numero"));
+    if(inserisciNumero % 2 == 0)
+        console.log(inserisciNumero);
+    else {
+        console.log(inserisciNumero + 1);
+    }
+}
