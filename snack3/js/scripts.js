847fd5a23a7c4cb3b904e62b6811096829f19e77
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-29 18:20:05 +0200

    commit

--- snack3/js/scripts.js
+++ snack3/js/scripts.js
@@ -1,24 +1,27 @@
 // esercizio 3
 
-// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
-// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi,
+// Gatsby vuole generare una falsa lista di 3 invitati.
 
 // lista nomi 
 var nomi = ["Ilario", "Mario", "Dario"];
 alert(nomi);
+
 // lista cognomi
 var cognomi = ["Giallo", "Verde", "Rosso"];
 alert(cognomi);
+
 // falsa lista
-var falsaLista= [];
-alert(falsaLista);
+var lista= [];
+alert(lista)
 
 
 for (var i = 0; i < 3; i++) {
     var nomiCasuali = Math.floor(Math.random() * nomi.length);
-    console.log(nomi);
-    var cognomiCasuali = Math.floor(Math.random() * 3);
-    console.log(cognomi);
-    if (nomi.lenght == nomiCasuali)
-        console.log(nomi);
+
+    var cognomiCasuali = Math.floor(Math.random() * cognomi.length);
+    
+    var falsaLista= nomi[nomiCasuali] + " " + cognomi[cognomiCasuali]; 
+    Lista.push(falsaLista);
 }
+console.log(lista);
